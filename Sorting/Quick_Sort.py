66718b03068dcfa9fdb56ede4bc7375b5c7dffe6
Pradhyumn72 <pradhyumn.gurjar1234@gmail.com>
2025-07-22 17:57:08 +0530

    quick sort

--- Sorting/Quick_Sort.py
+++ Sorting/Quick_Sort.py
@@ -1,29 +1,34 @@
-def partition(arr, low, high):
-    pivot = arr[low]
-    i = low
-    j = high
+# def partition(arr, low, high):
+#     pivot = arr[low]
+#     i = low
+#     j = high
 
-    while i < j:
-        while i <= high - 1 and arr[i] <= pivot:
-            i += 1
-        while j >= low + 1 and arr[j] > pivot:
-            j -= 1
-        if i < j:
-            arr[i], arr[j] = arr[j], arr[i]
+#     while i < j:
+#         while i <= high - 1 and arr[i] <= pivot:
+#             i += 1
+#         while j >= low + 1 and arr[j] > pivot:
+#             j -= 1
+#         if i < j:
+#             arr[i], arr[j] = arr[j], arr[i]
 
-    arr[low], arr[j] = arr[j], arr[low]
-    return j
+#     arr[low], arr[j] = arr[j], arr[low]
+#     return j
 
-def qs(arr, low, high):
-    if low < high:
-        p_index = partition(arr, low, high)
-        qs(arr, low, p_index - 1)
-        qs(arr, p_index + 1, high)
+# def qs(arr, low, high):
+#     if low < high:
+#         p_index = partition(arr, low, high)
+#         qs(arr, low, p_index - 1)
+#         qs(arr, p_index + 1, high)
 
-def quick_sort(arr):
-    qs(arr, 0, len(arr) - 1)
-    return arr
+# def quick_sort(arr):
+#     qs(arr, 0, len(arr) - 1)
+#     return arr
 
-arr=[3,2,9,1,7,4]
-result=quick_sort(arr)
-print(result)
+# arr=[3,2,9,1,7,4]
+# result=quick_sort(arr)
+# print(result)
+
+def partition():
+    pass
+def pivotindex():
+    pass
